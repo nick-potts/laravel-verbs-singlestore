5da89e0d004325bc08a3d04c309845262400c693
Nick Potts <github@nickpotts.com.au>
2024-06-28 09:24:36 +0800

    fix

--- app/script.php
+++ app/script.php
@@ -1,7 +1,7 @@
 <?php
 
-require './../vendor/autoload.php';
-$app = require_once '../bootstrap/app.php';
+require __DIR__ . '/../vendor/autoload.php';
+$app = require_once __DIR__. '/../bootstrap/app.php';
 $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
 $kernel->bootstrap();
 
